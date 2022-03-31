e16e89450eaac3b94beeebb691de1df5aec8e648
Shotz5 <kitsulalex@gmail.com>
2022-03-31 00:26:07 -0700

    Added Twitter oauth

--- api_pull.py
+++ api_pull.py
@@ -1,15 +1,83 @@
 import requests
-import json
 import html
-import os
 import sqlite3
+from requests_oauthlib import OAuth1Session
+from config import api_info
+import json
 
 def main():
-    conn = init_db()
-    pull_from_reddit(conn)
-    print(read_image_data(conn, "wj5d7830skq81.jpg"))
+    # conn = init_db()
+    # pull_latest_from_reddit(conn)
+    # print(read_image_data(conn, "wj5d7830skq81.jpg"))
 
-def pull_from_reddit(conn):
+    # twitter_oauth_generation()
+    auth = twitter_sign_in()
+
+    response = auth.post(
+        "https://api.twitter.com/2/tweets",
+        json = {"text": "Hello, world x3!"},
+    )
+
+    if response.status_code == 201:
+        print("Tweeted!")
+
+def twitter_oauth_generation():
+    consumer_key = api_info["api_key"]
+    consumer_secret = api_info["api_secret_key"]
+
+    request_token_url = "https://api.twitter.com/oauth/request_token?oauth_callback=oob&x_auth_access_type=write"
+    oauth = OAuth1Session(consumer_key, client_secret=consumer_secret)
+
+    try:
+        r = oauth.fetch_request_token(request_token_url)
+    except ValueError:
+        print("Invalid response from Twitter")
+        return
+
+    resource_owner_key = r.get('oauth_token')
+    resource_owner_secret = r.get('oauth_token_secret')
+    print("Got OAuth token: %s" % resource_owner_key)
+
+    base_authorization_url = "https://api.twitter.com/oauth/authorize"
+    authorization_url = oauth.authorization_url(base_authorization_url)
+    print("Please go here and authorize: %s" % authorization_url)
+    verifier = input('Please input the verifier: ')
+
+    access_token_url = "https://api.twitter.com/oauth/access_token"
+    oauth = OAuth1Session(consumer_key,
+                            client_secret=consumer_secret,
+                            resource_owner_key=resource_owner_key,
+                            resource_owner_secret=resource_owner_secret,
+                            verifier=verifier)
+
+    oauth_tokens = oauth.fetch_access_token(access_token_url)
+
+    access_token = oauth_tokens["oauth_token"]
+    access_token_secret = oauth_tokens["oauth_token_secret"]
+
+    with open("oauth_info.json", "w") as f:
+        json.dump({"consumer_key": consumer_key,
+                 "consumer_secret": consumer_secret,
+                 "access_token": access_token,
+                 "access_token_secret": access_token_secret}, f, indent=4)
+
+    print("Generated oauth link information")
+
+def twitter_sign_in():
+    with open("oauth_info.json", "r") as f:
+        oauth_info = f.read()
+        oauth_info = eval(oauth_info)
+
+    oauth = OAuth1Session(
+        oauth_info["consumer_key"],
+        client_secret=oauth_info["consumer_secret"],
+        resource_owner_key=oauth_info["access_token"],
+        resource_owner_secret=oauth_info["access_token_secret"],
+    )
+
+    return oauth
+
+def pull_latest_from_reddit(conn):
     api_url = "https://www.reddit.com/r/2meirl4meirl/hot/.json"
     headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36"}
     res = requests.get(api_url, headers=headers)
@@ -19,20 +87,21 @@ def pull_from_reddit(conn):
 
     for i in res_json["data"]["children"]:
         if "preview" in i["data"]:
+            l = []
             image_url = i["data"]["preview"]["images"][0]["source"]["url"]
+            op_url = i["data"]["permalink"]
             fixed_url = html.unescape(image_url)
-            image_urls.append(fixed_url)
+            l.append(fixed_url)
+            l.append(op_url)
+            image_urls.append(l)
 
     for i in image_urls:
-        file_name = i.split("/")[3].split(".")[0]
-        file_type = "." + i.split("/")[3].split(".")[1].split("?")[0]
+        file_name = i[0].split("/")[3].split(".")[0]
+        file_type = "." + i[0].split("/")[3].split(".")[1].split("?")[0]
         file_full_name = file_name + file_type
 
-        if (os.path.isfile("images/" + file_full_name) == False):
-            # with open("images/" + file_full_name, "wb") as f:
-            #     f.write(requests.get(i).content)
-            data = (file_full_name, i, False, False, False)
-            insert_image_data(conn, data)
+        data = (file_full_name, i[0], i[1], False, False, False)
+        insert_image_data(conn, data)
 
     conn.commit()
 
@@ -59,6 +128,7 @@ def create_table(conn):
     image_data_table  = """CREATE TABLE images (
                             file_name text PRIMARY KEY,
                             url text NOT NULL,
+                            op_url text NOT NULL,
                             posted boolean NOT NULL,
                             downloaded boolean NOT NULL,
                             deleted boolean NOT NULL
@@ -72,8 +142,8 @@ def create_table(conn):
         return False
 
 def insert_image_data(conn, data):
-    sql = ''' INSERT INTO images(file_name, url, posted, downloaded, deleted)
-              VALUES(?,?,?,?,?) '''
+    sql = ''' INSERT INTO images(file_name, url, op_url, posted, downloaded, deleted)
+              VALUES(?,?,?,?,?,?) '''
     try:
         cur = conn.cursor()
         cur.execute(sql, data)
